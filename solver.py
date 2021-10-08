2e13bb069bd626c9fa0d374c7567aad47795bf45
Brenton Horne <brentonhorne77@gmail.com>
2021-10-08 15:22:47 +1000

    +csv to gitignore; +solver script

---
+++ solver.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+import numpy as np
+import csv
+from scipy.stats import chi2
+from scipy.special import polygamma, gamma
+
+# Read the most important pieces of data from the CSV
+def readData(fileName, groupNo, depVarNo):
+    """
+    Returns group variable and dependent variable in fileName that are in the
+    columns specified by groupNo and depVarNo. 
+
+    Parameters
+    ----------
+    fileName : string.
+               The CSV file we're reading data from.
+    groupNo  : int.
+               An integer indicating the column in fileName in which our 
+               grouping variable is.
+    depVarNo : int.
+               An integer indicating the column in fileName in which our
+               dependent variable is.
+
+    Returns
+    -------
+    group    : NumPy array of integers.
+               Contains the grouping variable for each observation.
+    y        : NumPy array of floats.
+               Contains the dependent variable value for each observation.
+    """
+    # Initialize variables for reading from file
+    ifile = open(fileName)
+    reader = csv.reader(ifile)
+
+    # Initialize arrays to store variable data
+    y = np.array([])
+    group = np.array([])
+
+    # Initialize count for loop below
+    count = 0
+
+    # Loop through the rows in input file
+    for row in reader:
+        if ((depVarNo >= np.size(row)) or (groupNo >= np.size(row))):
+            print("At least one of the specified column numbers depVarNo or")
+            print("groupNo are greater than or equal to the number of columns")
+            print("in specified file.")
+            exit()
+        # Do not include headers
+        if (count != 0):
+            group = np.append(group, int(row[groupNo]))
+            y = np.append(y, float(row[depVarNo]))
+        count += 1
+
+    ifile.close()
+
+    return group, y
+
+def getVars(group, y):
+    """
+    Calculate various variables we need from group and y.
+
+    Parameters
+    ----------
+    group   : NumPy array of ints.
+              Group variable corresponding to each observation.
+    y       : NumPy array of floats.
+              Dependent variable value corresponding to each observation.
+
+    Returns
+    -------
+    m       : int.
+              Number of groups.
+    muNull  : NumPy array of floats.
+              Initial estimate of our MLE for the mean under the null.
+    varNull : NumPy array of floats.
+              Initial estimate of our MLE for the variance under the null.
+    nvec    : NumPy array of ints.
+              Vector of sample sizes for each value of the grouping variable.
+    yarr    : NumPy array of floats.
+              Array of values of the dependent variable for each observation 
+              with each row corresponding to a different value of the grouping
+              variable.
+    ybarvec : NumPy array of floats.
+              Means of the dependent variable for each value of the grouping 
+              variable.
+    """
+    # Number of groups
+    m = int(np.max(group))
+
+    # Vector of sample sizes
+    nvec = np.tile(0, m)
+    for i in range(0, m):
+        nvec[i] = int(np.size(y[group == i+1]))
+
+    nvec = np.reshape(nvec, (m, 1))
+
+    # Maximum sample size
+    ni = int(np.max(nvec))
+
+    # Initialize 2D array for storing y values categorized by treatment group
+    yarr = np.tile(0.5, (m, ni))
+
+    # yarr rows correspond to different groups
+    # columns different observations
+    for i in range(0, m):
+        for j in range (0, nvec[i,0]):
+            yarr[i, j] = y[group == i + 1][j]
+
+    # Ybar_i
+    ybarvec = np.mean(yarr, axis=1)
+    ybarvec = np.reshape(ybarvec, (m, 1))
+
+    # Initial guess of mu under the null
+    alphavec = 10*np.ones((m, 1))
+
+    return m, ni, alphavec, nvec, yarr, ybarvec
+
+def funjacUnr(alphavec, nvec, yarr, ybarvec):
+    Jvec = nvec * (1/alphavec - polygamma(1, alphavec))
+    Jinv = np.diagflat(1/Jvec)
+    #Jinv = np.diag(Jinv)
+    logsum = np.reshape(np.sum(np.log(yarr), axis=1), (m, 1))
+    F = -nvec * ( polygamma(0, alphavec) + np.log(ybarvec/alphavec)) + logsum
+
+    return Jinv, F
+
+def funjacNull(alpha, n, yarr, ybar):
+    J = n * (1/alpha - polygamma(1, alpha))
+    F = -n*(polygamma(0, alpha) + np.log(ybar/alpha)) + np.sum(np.log(yarr))
+    return J, F
+
+def newtonsUnr(m, alphavec, nvec, yarr, ybarvec):
+    Jinv, F = funjacUnr(alphavec, nvec, yarr, ybarvec)
+    eps = -np.matmul(Jinv, F)
+    diff = np.sqrt(np.sum(eps**2)/m)
+
+    iteration = 0
+    itMax = 1e3
+    tol = 1e-13
+    
+    while (tol < diff and iteration < itMax):
+        alphavec += eps
+        Jinv, F = funjacUnr(alphavec, nvec, yarr, ybarvec)
+        eps = -np.matmul(Jinv, F)
+        epsRel = eps / alphavec
+        diff = np.sqrt(np.sum(epsRel**2)/m)
+        iteration += 1
+
+    print("Number of iterations used to approximate alphavec = {}".format(iteration))
+    return alphavec
+
+def newtonsNull(alpha, n, yarr, ybar):
+    J, F = funjacNull(alpha, n, yarr, ybar)
+    eps = -F/J
+    
+    iteration = 0
+    itMax = 1e3
+    tol = 1e-13
+
+    while (tol < np.abs(eps)/alpha and iteration < itMax):
+        alpha += eps
+        J, F = funjacNull(alpha, n, yarr, ybar)
+        eps = -F/J
+        iteration += 1
+
+    print("Number of iterations used to approximate alpha = {}".format(iteration))
+    return alpha
+
+# alphavec's 2nd element is -inf when using OutlierRm
+#group, y = readData("ProjectDataOutlierRm.csv", 0, 4)
+# alphavec is fine when using original data set
+# But lam is nan due to gamma(alphavec) failing due to floating
+# point arithmetic limitations
+group, y = readData("ProjectData.csv", 0, 5)
+m, ni, alphavec, nvec, yarr, ybarvec = getVars(group, y)
+n = np.size(y)
+ybar = np.mean(yarr)
+alphavec = newtonsUnr(m, alphavec, nvec, yarr, ybarvec)
+betavec = ybarvec/alphavec
+alpha = 1
+alpha = newtonsNull(alpha, n, yarr, ybar)
+beta = ybar / alpha
+
+# For my dataset there is an alphavec entry = 260.2153579
+# Gamma(260.2153579) > 1e508, too big for ufuncs like scipy.special.gamma
+# to handle
+# I get the error:
+# TypeError: ufunc 'gamma' not supported for the input types, and the inputs 
+# could not be safely coerced to any supported types according to the casting 
+# rule ''safe''
+# When I convert alphavec to float128 type before running gamma on it
+lam = np.power(1/(gamma(alpha)*(ybar/alpha)**(alpha)), n)
+lam = lam * np.prod(np.power((gamma(alphavec) * np.power(ybarvec/alphavec, 
+alphavec)), nvec))
+lam *= np.prod(np.prod(np.power(yarr, alpha-alphavec), axis=1))
