8b487c4e1b91feff8a5b18bbb8e4bf991b115763
Brenton Horne <brentonhorne77@gmail.com>
2021-10-10 01:24:08 +1000

    Documenting Julia script; adding print lines

--- solver.jl
+++ solver.jl
@@ -1,6 +1,16 @@
 using StatsFuns, DecFP, CSV, DataFrames, Statistics, SpecialFunctions
 using LinearAlgebra
 
+"""
+    getVars(group::Vector, y::Vector)
+
+Computes various required variables from the grouping (group) and dependent 
+variables (y). Includes m, number of groups; n, total number of observations; 
+ni, maximum sample size; alphavec, m x 1 matrix of our initial guess for 
+``\\alpha_i``; nvec, m x 1 matrix of sample sizes for each group; yarr, 
+m x ni matrix of observations; ybar, the overall mean for all observations;
+and ybarvec, m x 1 matrix of group means.
+"""
 function getVars(group, y)
     m = maximum(group)
     nvec = zeros((m, 1))
@@ -8,6 +18,7 @@ function getVars(group, y)
         nvec[i, 1] = length(y[isequal.(group, i)])
     end
     nvec = Int.(nvec)
+    n = length(y)
     ni = maximum(nvec)
     yarr = BigFloat.(zeros((m, ni)))
     
@@ -17,13 +28,20 @@ function getVars(group, y)
         end
     end
 
+    ybar = mean(y)
     ybarvec = mean(yarr, dims=2)
     ybarvec = reshape(ybarvec, m, 1)
     alphavec = 10 * ones((m, 1))
 
-    return m, ni, alphavec, nvec, yarr, ybarvec
+    return m, n, ni, alphavec, nvec, yarr, ybar, ybarvec
 end
 
+"""
+    funjacUnr(alphavec::Matrix, nvec::Matrix, yarr::Matrix, ybarvec::Matrix)
+
+Computes and returns the inverse of the Jacobian and the matrix of function 
+values for computing the unrestricted MLE of alpha_i.
+"""
 function funjacUnr(alphavec, nvec, yarr, ybarvec)
     Jinv = Diagonal(vec((nvec .* (alphavec.^(-1)-polygamma.(1, Float64.(alphavec)))).^(-1)))
     logsum = reshape(sum(log.(yarr), dims=2), (m, 1))
@@ -31,13 +49,25 @@ function funjacUnr(alphavec, nvec, yarr, ybarvec)
     return Jinv, F
 end
 
+"""
+    funjacNull(alpha::Float64, n::Int64, yarr::Matrix, ybar::Matrix)
+
+Computes and returns the derivative and function value required to compute
+the MLE of alpha under the null hypothesis.
+"""
 function funjacNull(alpha, n, yarr, ybar)
     J = n * (1/alpha - polygamma(1, Float64(alpha)))
     F = -n * (polygamma(0, Float64(alpha)) + log(ybar/alpha)) + sum(log.(yarr))
     return J, F
 end
 
-function newtonsUnr(m, alphavec, nvec, yarr, ybarvec)
+"""
+    newtonsUnr(m::Int64, alphavec::Matrix, nvec::Matrix, yarr::Matrix, 
+    ybarvec::Matrix, itMax::Int64, tol::Float64)
+
+Estimates the unrestricted MLE of alpha_i using Newton's method.
+"""
+function newtonsUnr(m, alphavec, nvec, yarr, ybarvec, itMax, tol)
     Jinv, F = funjacUnr(alphavec, nvec, yarr, ybarvec)
     eps = -Jinv * F
     diff = sqrt(sum(eps.^2)/m)
@@ -57,7 +87,13 @@ function newtonsUnr(m, alphavec, nvec, yarr, ybarvec)
     return alphavec
 end
 
-function newtonsNull(alpha, n, yarr, ybar)
+"""
+    newtonsNull(alpha::Float64, n::Int64, yarr::Matrix, ybar::Float64, 
+    itMax::Int64, tol::Float64)
+
+Estimates the MLE of alpha under the null using Newton's method.
+"""
+function newtonsNull(alpha, n, yarr, ybar, itMax, tol)
     J, F = funjacNull(alpha, n, yarr, ybar)
     eps = -F/J
     
@@ -75,21 +111,39 @@ function newtonsNull(alpha, n, yarr, ybar)
     return alpha
 end
 
+# Get problem data and parameters
 csv_reader = CSV.File("ProjectData.csv")
 dataF = DataFrame(csv_reader)
 y = BigFloat.(dataF[:, 6])
 group = dataF[:, 1]
-m, ni, alphavec, nvec, yarr, ybarvec = getVars(group, y)
-n = length(y)
-ybar = mean(y)
+m, n, ni, alphavec, nvec, yarr, ybar, ybarvec = getVars(group, y)
+
+# Estimate unrestricted MLEs
 alphavec = newtonsUnr(m, alphavec, nvec, yarr, ybarvec)
-betavec = ybarvec/alphavec
+betavec = alphavec.^(-1) .* ybarvec
+
+# Estimate MLEs under null
 alpha = 1
 alpha = newtonsNull(alpha, n, yarr, ybar)
 beta = ybar/alpha
 
+# Likelihood ratio
 lam = (gamma(alpha) * (ybar/alpha)^(alpha))^(-n)
 lam *= prod(prod(yarr.^(alpha*ones(length(alphavec), 1)-alphavec), dims=2))
 lam *= prod((gamma.(alphavec) .* (ybarvec.*alphavec.^(-1)).^(alphavec)).^(nvec))
+
+# Test statistic, -2 ln(lambda)
 stat = -2*log(lam)
+
+# Obtain p-value keeping in mind that under the null our test statistic
+# should asymptotically follow a chi-squared distribution with 2m-2 df
 pval = 1-chisqcdf(2*m-2, Float64(stat))
+
+# Printing important data
+println("alpha (null)   = ", Float64(alpha))
+println("beta (null)    = ", Float64(beta))
+println("alpha_i        = ", Float64.(alphavec))
+println("beta_i         = ", Float64.(betavec))
+println("Lambda         = ", lam)
+println("Test statistic = ", Float64(stat))
+println("P-value        = ", pval)
